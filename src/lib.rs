027583271c08d78036feea982ed04f2a7272ad0d
bruderj15 <julian.bruder@outlook.com>
2025-05-01 20:23:19 +0200

    main: mindful error handling when deserializing

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,54 @@
-use aws_lambda_events::sqs::SqsEvent;
+use aws_lambda_events::sqs::{BatchItemFailure, SqsBatchResponse, SqsEvent};
 use aws_sdk_dynamodb::Client;
 use item_core::item_data::ItemData;
-use item_core::item_model::ItemModel;
 use item_write::write_items;
 use lambda_runtime::{Error, LambdaEvent};
-use serde_json::{from_str};
-use tracing::info;
+use serde_json::from_str;
+use tracing::{info, warn};
 
 #[tracing::instrument(skip(event), fields(req_id = %event.context.request_id))]
-pub async fn function_handler(client: &Client, event: LambdaEvent<SqsEvent>) -> Result<(), Error> {
-    let items = event
-        .payload
-        .records
+pub async fn function_handler(
+    client: &Client,
+    event: LambdaEvent<SqsEvent>,
+) -> Result<SqsBatchResponse, Error> {
+    let mut batch_item_failures = Vec::new();
+    let mut items = Vec::new();
+
+    for item_rec in event.payload.records {
+        let message_id = item_rec
+            .message_id
+            .expect("should never receive an SQS-Message without message_id because AWS sets it.");
+
+        match item_rec.body {
+            None => {
+                warn!("Received empty body for message_id '{message_id}'");
+                batch_item_failures.push(BatchItemFailure {
+                    item_identifier: message_id,
+                });
+            }
+            Some(ref item_json) => match from_str::<ItemData>(item_json) {
+                Ok(item_datum) => items.push(item_datum.into()),
+                Err(e) => {
+                    warn!(
+                        "Deserialization error for message_id '{message_id}': {e}. Body: '{item_json}'"
+                    );
+                    batch_item_failures.push(BatchItemFailure {
+                        item_identifier: message_id,
+                    });
+                }
+            },
+        }
+    }
+
+    write_items(&items, client)
+        .await
         .into_iter()
-        .filter_map(|datum| datum.body)
-        .map(|item_datum_json| from_str::<ItemData>(&item_datum_json))
-        // TODO: Don't swallow error, handle
-        .filter_map(|item_datum| item_datum.ok())
-        .map(ItemData::into)
-        .collect::<Vec<ItemModel>>();
+        .for_each(|res| {
+            info!("{:?}", res);
+            todo!()
+        });
 
-    write_items(&items, client).await.into_iter().for_each(|res| {
-        info!("{:?}", res);
-    });
-
-    info!("Wrote items, finished invocation");
-    
-    // TODO: Retries, DLQ, partial failure, ...
-
-    Ok(())
+    Ok(SqsBatchResponse {
+        batch_item_failures,
+    })
 }
